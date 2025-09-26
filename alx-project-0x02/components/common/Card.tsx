60e584a55bd592366a39e957ca955d336d4d9888
maniel <manuelpepr99@icloud.com>
2025-09-26 17:44:07 +0000

    Create a Reusable Card Component

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,11 @@
+import { CardProps } from "@/interfaces";
+
+export default function Card({title, content}: CardProps){
+    return (
+        <>
+            <h1>{title}</h1>
+            <h1>{content}</h1>
+        </>
+       
+    )
+}
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Card from "@/components/common/Card";
+
 export default function(){
     return (
         <>
           <h1>welcome to home page</h1>
+          <Card title="Property 1" content="choose this fast" />
         </>
     )
 }
