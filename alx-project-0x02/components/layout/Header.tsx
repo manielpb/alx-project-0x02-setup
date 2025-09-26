3dfa267a8f54354ae61ae19a369bd778fedd8923
maniel <manuelpepr99@icloud.com>
2025-09-26 17:21:33 +0000

    Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,17 @@
-import React from 'react'
+import React from 'react';
+import about from '@/pages/about';
+import home from '@/pages/home';
+
+export default function Header(){
+
+    return(
+        <>
+            <header>
+                <nav>
+                    <a href='home'>Home</a>
+                    <a href='about'>About</a>
+                </nav>
+            </header>
+        </>
+    )
+}
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function(){
+    return (
+        <>
+          <h1>welcome to about page</h1>
+        </>
+    )
+}
