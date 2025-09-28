7889a5f819c5a893827bb3535a33b617b458ff68
maniel <manuelpepr99@icloud.com>
2025-09-28 16:19:09 +0000

    update

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
+import { useEffect, useState } from "react";
+const url = "https://jsonplaceholder.typicode.com";
 
 export default function(){
-    return (
-        <>
-            <Header/>
-        </>
-    )
+    useEffect ( () => {
+        const fetchPosts = async () => {
+            const response = await fetch(`${url}/posts`);
+            const posts = await response.json();
+        };
+    })
 }
