56f3a5f3a4eb3a6e3028b2552eab636169f29855
maniel <manuelpepr99@icloud.com>
2025-09-28 16:57:33 +0000

    Implement a PostCard Component

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -1,5 +1,13 @@
 import { PostProps } from "@/interfaces";
+import Posts from "@/pages/posts";
+import { useState } from "react";
 
-export default function({title, content, userId}: PostProps){
-    
+export default function PostCard({title, content, userId}: PostProps){
+    return(
+        <div className="border p-4 rounded-md shadow-sm mb-4">
+            <h2 className="text-lg font-bold">{title}</h2>
+            <p className="text gray-600">{content}</p>
+            <span className="text-sm text-gray-400">Posted by user {userId}</span>
+        </div>
+    )
 }
--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,13 +1,45 @@
+import { GetStaticProps } from "next";
 import Header from "@/components/layout/Header";
 import { PostProps } from "@/interfaces";
-import { useEffect, useState } from "react";
-const url = "https://jsonplaceholder.typicode.com";
+import PostCard from "@/components/common/PostCard";
 
-export default function(){
-    useEffect ( () => {
-        const fetchPosts = async () => {
-            const response = await fetch(`${url}/posts`);
-            const posts = await response.json();
-        };
-    })
+type PostsPageProps = {
+  posts: PostProps[];
+};
+
+export default function PostsPage({ posts }: PostsPageProps) {
+
+  return (
+    <>
+    <Header />
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Posts</h1>
+      {posts.map((post) => (
+        <PostCard
+          key={post.title} 
+          title={post.title}
+          content={post.content}
+          userId={post.userId}
+        />
+      ))}
+    </div>
+    </>
+  );
+
 }
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  const posts: PostProps[] = data.map((post: any) => ({
+    userId: post.userId,
+    title: post.title,
+    content: post.body, 
+  }));
+
+  return {
+    props: { posts },
+  };
+};
